b0afb619aeeda79a0fc4eb09cc8a894bdaa031ee
Tony <ANCLIST@GMAIL.COM>
2017-10-17 14:48:33 -0400

    Generate and run migration for the location table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017183719) do
+ActiveRecord::Schema.define(version: 20171017184541) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string  "name"
+    t.decimal "phone_number"
+    t.text    "address"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
