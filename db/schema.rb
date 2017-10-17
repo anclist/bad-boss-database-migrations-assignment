094f9299a67be0a6baba85631a605584db575fd5
Tony <ANCLIST@GMAIL.COM>
2017-10-17 14:35:21 -0400

    Modify the parts table to add the quantity column, create and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017182430) do
+ActiveRecord::Schema.define(version: 20171017183029) do
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
+    t.integer "quantity"
   end
 
 end
